700835e0da41237555a62691a152f2545d29c73f
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2018-07-24 15:06:01 +0200

    turn GstOMXComponent to a GstMiniObject
    Will use it for refcounting.
    
    Upstream-Status: Pending
    Signed-off-by: Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>

--- omx/gstomx.h
+++ omx/gstomx.h
@@ -322,6 +322,8 @@ struct _GstOMXPort {
 };
 
 struct _GstOMXComponent {
+  GstMiniObject mini_object;
+
   GstObject *parent;
 
   gchar *name; /* for debugging mostly */
@@ -408,7 +410,8 @@ void              gst_omx_core_release (GstOMXCore * core);
 
 
 GstOMXComponent * gst_omx_component_new (GstObject * parent, const gchar *core_name, const gchar *component_name, const gchar * component_role, guint64 hacks);
-void              gst_omx_component_free (GstOMXComponent * comp);
+GstOMXComponent * gst_omx_component_ref   (GstOMXComponent * comp);
+void              gst_omx_component_unref (GstOMXComponent * comp);
 
 OMX_ERRORTYPE     gst_omx_component_set_state (GstOMXComponent * comp, OMX_STATETYPE state);
 OMX_STATETYPE     gst_omx_component_get_state (GstOMXComponent * comp, GstClockTime timeout);
