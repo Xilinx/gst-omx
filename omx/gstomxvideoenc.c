f19eede8351cf165a8c6ede06b0de1b5f3eb7ef2
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2018-08-13 15:10:37 +0200

    omx: allow gst_omx_port_acquire_buffer() to not wait for buffers
    Will be needed to implement GST_BUFFER_POOL_ACQUIRE_FLAG_DONTWAIT.
    
    Upstream-Status: Pending
    Signed-off-by: Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>

--- omx/gstomxvideoenc.c
+++ omx/gstomxvideoenc.c
@@ -1655,7 +1655,7 @@ gst_omx_video_enc_loop (GstOMXVideoEnc * self)
 
   klass = GST_OMX_VIDEO_ENC_GET_CLASS (self);
 
-  acq_return = gst_omx_port_acquire_buffer (port, &buf);
+  acq_return = gst_omx_port_acquire_buffer (port, &buf, GST_OMX_WAIT);
   if (acq_return == GST_OMX_ACQUIRE_BUFFER_ERROR) {
     goto component_error;
   } else if (acq_return == GST_OMX_ACQUIRE_BUFFER_FLUSHING) {
@@ -3074,7 +3074,7 @@ gst_omx_video_enc_handle_frame (GstVideoEncoder * encoder,
       fill_buffer = FALSE;
       buf->omx_buf->nFilledLen = gst_buffer_get_size (frame->input_buffer);
     } else {
-      acq_ret = gst_omx_port_acquire_buffer (port, &buf);
+      acq_ret = gst_omx_port_acquire_buffer (port, &buf, GST_OMX_WAIT);
     }
 
     if (acq_ret == GST_OMX_ACQUIRE_BUFFER_ERROR) {
@@ -3346,7 +3346,7 @@ gst_omx_video_enc_drain (GstOMXVideoEnc * self)
   /* Send an EOS buffer to the component and let the base
    * class drop the EOS event. We will send it later when
    * the EOS buffer arrives on the output port. */
-  acq_ret = gst_omx_port_acquire_buffer (self->enc_in_port, &buf);
+  acq_ret = gst_omx_port_acquire_buffer (self->enc_in_port, &buf, GST_OMX_WAIT);
   if (acq_ret != GST_OMX_ACQUIRE_BUFFER_OK) {
     GST_VIDEO_ENCODER_STREAM_LOCK (self);
     GST_ERROR_OBJECT (self, "Failed to acquire buffer for draining: %d",
