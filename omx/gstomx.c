284fee7b989a3976b008029c51e7347da2519f6c
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2019-07-09 15:58:47 +0200

    omx: add new index to omx_index_type_to_str()

--- omx/gstomx.c
+++ omx/gstomx.c
@@ -1968,6 +1968,12 @@ omx_index_type_to_str (OMX_INDEXTYPE index)
       return "OMX_ALG_IndexParamVideoInputParsed";
     case OMX_ALG_IndexParamVideoMaxPictureSize:
       return "OMX_ALG_IndexParamVideoMaxPictureSize";
+    case OMX_ALG_IndexParamVideoMaxPictureSizes:
+      return "OMX_ALG_IndexParamVideoMaxPictureSizes";
+    case OMX_ALG_IndexConfigVideoLoopFilterBeta:
+      return "OMX_ALG_IndexConfigVideoLoopFilterBeta";
+    case OMX_ALG_IndexConfigVideoLoopFilterTc:
+      return "OMX_ALG_IndexConfigVideoLoopFilterTc";
     case OMX_ALG_IndexMaxEnum:
       return "OMX_ALG_IndexMaxEnum";
   }
