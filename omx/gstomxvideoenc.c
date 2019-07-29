b89924ea5c1d9a7903f2870062f62dffdc8613a2
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2019-07-29 15:37:10 +0530

    omxvideoenc: XLNX-LL: send custom event when ready
    Upstream-Status: Invalid [Xilinx specific]
    Signed-off-by: Guillaume Desmottes <guillaume.desmottes@collabora.com>

--- omx/gstomxvideoenc.c
+++ omx/gstomxvideoenc.c
@@ -2146,6 +2146,7 @@ gst_omx_video_enc_start (GstVideoEncoder * encoder)
   self->nb_upstream_buffers = 0;
   self->nb_downstream_buffers = 0;
   self->in_pool_used = FALSE;
+  self->xlnx_ll = FALSE;
 
   return TRUE;
 }
@@ -2938,6 +2939,18 @@ gst_omx_video_enc_set_format (GstVideoEncoder * encoder,
   gst_omx_video_enc_set_latency (self);
   if (!gst_omx_video_enc_set_color_primaries (self))
     return FALSE;
+
+  {
+    GstCapsFeatures *features;
+
+    features = gst_caps_get_features (state->caps, 0);
+    if (features
+        && gst_caps_features_contains (features,
+            GST_CAPS_FEATURE_MEMORY_XLNX_LL)) {
+      GST_DEBUG_OBJECT (self, "Input is using XLNX-LowLatency");
+      self->xlnx_ll = TRUE;
+    }
+  }
 #endif
 
   self->downstream_flow_ret = GST_FLOW_OK;
@@ -3297,6 +3310,7 @@ gst_omx_video_enc_handle_frame (GstVideoEncoder * encoder,
   GstOMXBuffer *buf;
   OMX_ERRORTYPE err;
   GstClockTimeDiff deadline;
+  gboolean starting = FALSE;
 
   self = GST_OMX_VIDEO_ENC (encoder);
 
@@ -3326,6 +3340,7 @@ gst_omx_video_enc_handle_frame (GstVideoEncoder * encoder,
     GST_DEBUG_OBJECT (self, "Starting task");
     gst_pad_start_task (GST_VIDEO_ENCODER_SRC_PAD (self),
         (GstTaskFunction) gst_omx_video_enc_loop, self, NULL);
+    starting = TRUE;
   }
 
   port = self->enc_in_port;
@@ -3513,6 +3528,19 @@ gst_omx_video_enc_handle_frame (GstVideoEncoder * encoder,
   }
 
   gst_video_codec_frame_unref (frame);
+
+#ifdef USE_OMX_TARGET_ZYNQ_USCALE_PLUS
+  if (self->xlnx_ll && starting) {
+    GstEvent *event;
+
+    GST_DEBUG_OBJECT (self, "Tell XLNX-LL producer it can start streaming");
+
+    event = gst_event_new_custom (GST_EVENT_CUSTOM_UPSTREAM,
+        gst_structure_new ("xlnx-ll-consumer-ready", NULL, NULL));
+
+    gst_pad_push_event (GST_VIDEO_DECODER_SINK_PAD (self), event);
+  }
+#endif
 
   return self->downstream_flow_ret;
 
