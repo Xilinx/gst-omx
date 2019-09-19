476e95293c532cb0c7d99ab4c5615f26262d750b
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2019-09-19 15:07:47 +0530

    omxvideodec: zynq: use seamless input/output transition if possible
    The Zynq OMX is able to do seamless resolution changes as an
    optimization, preventing a full reconfiguration of the port and
    re-allocation of its buffers.
    
    So if we detect that only the resolution changed and the existing
    buffers are large enough to hold the upcoming frames, don't do a
    reconfiguration of the port and just update the src caps.
    
    Upstream-Status: Pending
    Signed-off-by: Guillaume Desmottes <guillaume.desmottes@collabora.com>
    Signed-off-by: Stéphane Cerveau <scerveau@collabora.com>

--- omx/gstomxvideo.h
+++ omx/gstomxvideo.h
@@ -73,6 +73,8 @@ gboolean gst_omx_video_get_port_padding (GstOMXPort * port, GstVideoInfo * info_
 
 #ifdef USE_OMX_TARGET_ZYNQ_USCALE_PLUS
 GstCaps * gst_omx_video_add_xlnx_ll_to_caps (GstCaps * caps, gboolean encoder);
+
+gboolean gst_omx_video_port_support_resolution (GstOMXPort * port, guint width, guint height);
 #endif
 
 G_END_DECLS
