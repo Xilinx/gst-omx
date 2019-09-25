7a5e5f0c2c04d69e9207598fee5fafc6368ac6f0
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2019-09-25 10:49:14 +0200

    omx: omx_index_type_to_str: add OMX_ALG_IndexConfigVideoMaxResolutionChange
    Upstream-Status: Pending
    Signed-off-by: Guillaume Desmottes <guillaume.desmottes@collabora.com>

--- omx/gstomx.c
+++ omx/gstomx.c
@@ -2003,6 +2003,8 @@ omx_index_type_to_str (OMX_INDEXTYPE index)
       return "OMX_ALG_IndexConfigVideoColorMatrix";
     case OMX_ALG_IndexConfigVideoHighDynamicRangeSEIs:
       return "OMX_ALG_IndexConfigVideoHighDynamicRangeSEIs";
+    case OMX_ALG_IndexConfigVideoMaxResolutionChange:
+      return "OMX_ALG_IndexConfigVideoMaxResolutionChange";
     case OMX_ALG_IndexMaxEnum:
       return "OMX_ALG_IndexMaxEnum";
   }
